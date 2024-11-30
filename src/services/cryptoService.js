a34bc002c8462c83ea4af8f50bc11bdf2d77d89d
NorlamRS17 <marlon.mp.18@hotmail.com>
2024-11-30 17:22:55 -0500

    Frontend

--- src/services/cryptoService.js
+++ src/services/cryptoService.js
@@ -1,13 +1,20 @@
 import axios from 'axios'
 
-const API_URL = 'http://localhost:5000/cryptos';
+// Usamos CORS-Anywhere como proxy
+const PROXY_URL = 'https://cors-anywhere.herokuapp.com/';
+const API_URL = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest';
+const API_KEY = '3c600a51-9285-4a26-938d-daa34d9ee3b7';
 
 export const AxioCryptos = async () => {
   try {
-    const response = await axios.get(API_URL); //LLAMA AL SERVIDOR BACKEND "crypto-backend"
-    return response.data; 
+    const response = await axios.get(PROXY_URL + API_URL, {
+      headers: {
+        'X-CMC_PRO_API_KEY': API_KEY,
+      },
+    });
+    return response.data.data; // Devuelve los datos de criptomonedas
   } catch (error) {
     console.error('Error fetching cryptocurrencies:', error);
-    return []; 
+    return []; // Devuelve un array vacío en caso de error
   }
 };
